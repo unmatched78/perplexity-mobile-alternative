0a37435e51cb8933428582306a109ef3c997bba3
unmatched78 <marvinavi24@gmail.com>
2025-09-22 12:42:42 +0200

    Add a new StatelessWidget class for future UI development

--- lib/main.dart
+++ lib/main.dart
@@ -3,3 +3,11 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
+class name extends StatelessWidget {
+  const name({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
