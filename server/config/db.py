47228d8ba294f5a2ca578bee1670294bb64cbce7
unmatched78 <marvinavi24@gmail.com>
2025-09-24 23:19:09 +0200

    server/config/db:

--- server/config/db.py
+++ server/config/db.py
@@ -1,3 +1,7 @@
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
+
+engine = create_engine('sqlite:///./test.db', connect_args={"check_same_thread": False})
+SessionLocal = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
+Base = declarative_base()
