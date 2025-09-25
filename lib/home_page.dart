8176f2409136e946752b8abb2666cac6d9de48ec
unmatched78 <marvinavi24@gmail.com>
2025-09-25 20:06:52 +0200

    lib/home_page:

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -2,9 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flut/widgets/sidebar.dart';
 import 'package:flut/theme/colors.dart';
 import 'package:flut/widgets/search_section.dart';
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
