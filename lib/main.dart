c97cfe8bc9cc322c4cae0b8d0ceec90508fde511
unmatched78 <marvinavi24@gmail.com>
2025-09-25 21:00:35 +0200

    lib/main:

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flut/home_page.dart';
-import 'package:flut/theme/colors.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:perplexity_clone/pages/home_page.dart';
+import 'package:perplexity_clone/theme/colors.dart';
+
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -11,15 +14,20 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Perplexity Clone',
       theme: ThemeData(
-        scaffoldBackgroundColor: AppColors:AppColors.background,
-        textTheme: GoogleFonts.interTextTheme(),
-        ThemeData.dark().textTheme,
-        
+        scaffoldBackgroundColor: AppColors.background,
+        colorScheme: ColorScheme.fromSeed(seedColor: AppColors.submitButton),
+        textTheme: GoogleFonts.interTextTheme(
+          ThemeData.dark().textTheme.copyWith(
+                bodyMedium: const TextStyle(
+                  fontSize: 15,
+                  color: AppColors.whiteColor,
+                ),
+              ),
+        ),
       ),
-
-      home: const HomePage(),
+      home: HomePage(),
     );
   }
 }
