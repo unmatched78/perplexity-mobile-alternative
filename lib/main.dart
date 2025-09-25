1914a90343e9ed8c5fe899d07e7464d25cb0222e
unmatched78 <marvinavi24@gmail.com>
2025-09-25 11:38:32 +0200

    lib/main:

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,10 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-class name extends StatelessWidget {
-  const name({super.key});
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: const HomePage(),
-    );
+    return 
   }
 }
