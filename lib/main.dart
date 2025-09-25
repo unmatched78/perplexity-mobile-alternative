1219844d1ae1435291b5cecbaa0feda7e2c2c19f
unmatched78 <marvinavi24@gmail.com>
2025-09-25 12:33:16 +0200

    lib/main:

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         scaffoldBackgroundColor: AppColors:AppColors.background,
+        textTheme: GoogleFonts.interTextTheme(),
       ),
 
       home: const HomePage(),
