206bdae0264894c481539fede3761c4a72189f3a
unmatched78 <marvinavi24@gmail.com>
2025-09-25 13:23:26 +0200

    lib/widgets/search_bar_button: create

---
+++ lib/widgets/search_bar_button.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SearchBarButtoon extends StatefulWidget {
+  const SearchBarButtoon({super.key});
+
+  @override
+  State<SearchBarButtoon> createState() => _SearchBarButtoonState();
+}
+
+class _SearchBarButtoonState extends State<SearchBarButtoon> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
