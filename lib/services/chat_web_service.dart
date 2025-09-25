9705290da678fe8da5fcf5575b53bd60f1352070
unmatched78 <marvinavi24@gmail.com>
2025-09-25 20:05:24 +0200

    lib/services/chat_web_service:

--- lib/services/chat_web_service.dart
+++ lib/services/chat_web_service.dart
@@ -1,8 +1,16 @@
+import 'dart:convert';
+
 import 'package:web_socket_client/web_socket_client.dart';
 class ChatWebService {
 
   void connect() {
     // Connect to chat service
+    _socket =WebSocket(Uri.parse("ws://localhost:8000/ws/chat"));
+    _socket!.messages.listen((message) {
+      // Handle incoming messages
+      final data =json.encode(message);
+      print("Received message: $data"); 
+    });
   }
   void chat() {
     // Chat functionality
