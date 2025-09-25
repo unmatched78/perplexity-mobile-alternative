62e6e1509befe0ce72c20d61e4bb21f8253d544a
unmatched78 <marvinavi24@gmail.com>
2025-09-25 11:43:24 +0200

    lib/widgets/sidebar: create

---
+++ lib/widgets/sidebar.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flut/theme/colors.dart';
+class Sidebar extends StatelessWidget {
+  const Sidebar({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+      width:64,
+      color: AppColors.sideNav,
+    );
+  }
+}
