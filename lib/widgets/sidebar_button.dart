bc639884420292f8e92e773363454a7cc5fb62cd
unmatched78 <marvinavi24@gmail.com>
2025-09-25 12:46:45 +0200

    lib/widgets/sidebar_button:

--- lib/widgets/sidebar_button.dart
+++ lib/widgets/sidebar_button.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
-
+import 'package:flut/theme/colors.dart';
 class SidebarButton extends StatelessWidget {
   const SidebarButton({super.key});
 
   @override
   Widget build(BuildContext context) {
-    bool isCollapsed = true;
+    final isCollapsed = true;
+    final IconData icon;
+    final Text text;
+    
+
+
     return Row(
             mainAxisAlignment: isCollapsed ? MainAxisAlignment.center: MainAxisAlignment.start,
             children: [
