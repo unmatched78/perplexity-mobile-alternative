ae1f30f5dc6b903fb37f21b59814bd05b3c1233a
unmatched78 <marvinavi24@gmail.com>
2025-09-25 20:53:47 +0200

    lib/pages/home_page:

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,10 @@
-import 'package:flut/services/chat_web_service.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flut/widgets/sidebar.dart';
-import 'package:flut/theme/colors.dart';
-import 'package:flut/widgets/search_section.dart';
+import 'package:perplexity_clone/services/chat_web_service.dart';
+import 'package:perplexity_clone/theme/colors.dart';
+import 'package:perplexity_clone/widgets/search_section.dart';
+import 'package:perplexity_clone/widgets/side_bar.dart';
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -16,16 +18,95 @@ class _HomePageState extends State<HomePage> {
     super.initState();
     ChatWebService().connect();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:Row(children: [
-        Sidebar(),
-        Column(children: [
-          Expanded(child: SearchSection(),),
-          Container(color:AppColors.background, width:600, height:400,)
-        ],),
-      ],)
-    )
+        body: Row(
+      children: [
+        kIsWeb ? SideBar() : SizedBox(),
+        Expanded(
+          child: Padding(
+            padding: !kIsWeb ? const EdgeInsets.all(8.0) : EdgeInsets.zero,
+            child: Column(
+              children: [
+                Expanded(
+                  child: SearchSection(),
+                ),
+                // footer
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 16),
+                  child: Wrap(
+                    alignment: WrapAlignment.center,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 12),
+                        child: Text(
+                          "Pro",
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: AppColors.footerGrey,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 12),
+                        child: Text(
+                          "Enterprise",
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: AppColors.footerGrey,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 12),
+                        child: Text(
+                          "Store",
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: AppColors.footerGrey,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 12),
+                        child: Text(
+                          "Blog",
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: AppColors.footerGrey,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 12),
+                        child: Text(
+                          "Careers",
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: AppColors.footerGrey,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 12),
+                        child: Text(
+                          "English (English)",
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: AppColors.footerGrey,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        )
+      ],
+    ));
   }
 }
