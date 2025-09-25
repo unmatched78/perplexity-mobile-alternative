ec6157a906ea56c662909023b5a0bc7404b4be6b
unmatched78 <marvinavi24@gmail.com>
2025-09-25 20:54:45 +0200

    lib/services/chat_web_service:

--- lib/services/chat_web_service.dart
+++ lib/services/chat_web_service.dart
@@ -1,18 +1,37 @@
+import 'dart:async';
 import 'dart:convert';
-import 'package:web_socket_Client/web_socket_client.dart';
+import 'package:web_socket_client/web_socket_client.dart';
+
 class ChatWebService {
+  static final _instance = ChatWebService._internal();
   WebSocket? _socket;
+
+  factory ChatWebService() => _instance;
+
+  ChatWebService._internal();
+  final _searchResultController = StreamController<Map<String, dynamic>>();
+  final _contentController = StreamController<Map<String, dynamic>>();
+
+  Stream<Map<String, dynamic>> get searchResultStream =>
+      _searchResultController.stream;
+  Stream<Map<String, dynamic>> get contentStream => _contentController.stream;
+
   void connect() {
-    // Connect to chat service
-    _socket =WebSocket(Uri.parse("ws://localhost:8000/ws/chat"));
+    _socket = WebSocket(Uri.parse("ws://localhost:8000/ws/chat"));
+
     _socket!.messages.listen((message) {
-      // Handle incoming messages
-      final data =json.decode(message);
-      print("Received message: $data"); 
+      final data = json.decode(message);
+      if (data['type'] == 'search_result') {
+        _searchResultController.add(data);
+      } else if (data['type'] == 'content') {
+        _contentController.add(data);
+      }
     });
   }
+
   void chat(String query) {
-    // Chat functionality
-    _socket?.add(json.encode({"message": "Hello, World!"}));
+    print(query);
+    print(_socket);
+    _socket!.send(json.encode({'query': query}));
   }
 }
