178a9a1bfb35c7fa318975fa8aee6a0128aac237
unmatched78 <marvinavi24@gmail.com>
2025-09-25 13:01:28 +0200

    lib/widgets/search_section: create

---
+++ lib/widgets/search_section.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flut/theme/colors.dart';
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
