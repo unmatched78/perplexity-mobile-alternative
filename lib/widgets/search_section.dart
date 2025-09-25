b35d56b6d6bef9d9721993ab3be53dc169c399ae
unmatched78 <marvinavi24@gmail.com>
2025-09-25 13:04:44 +0200

    lib/widgets/search_section:

--- lib/widgets/search_section.dart
+++ lib/widgets/search_section.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flut/theme/colors.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+class SearchSection extends StatelessWidget {
+  const SearchSection({super.key});
 
   @override
   Widget build(BuildContext context) {
